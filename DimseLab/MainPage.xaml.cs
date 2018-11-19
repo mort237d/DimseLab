5e7ab1745aa1e718194a8ce1e56e0a9f1971e4d0
Morten <morto1995@hotmail.com>
2018-11-19 09:45:05 +0100

    Titlebar color changed

--- DimseLab/MainPage.xaml.cs
+++ DimseLab/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,9 @@ namespace DimseLab
         public MainPage()
         {
             this.InitializeComponent();
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = Color.FromArgb(Byte.MaxValue, 109, 174, 219);
         }
     }
 }
